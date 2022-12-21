27a80a0f8b78aedbf235a84ae704aa5d7d09712b
sneakycrow <zach@sneakycrow.dev>
2022-12-21 15:57:02 -0800

    adds Spa rendering with the axum library

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::Router;
+use axum_extra::routing::SpaRouter;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+    let app: Router = Router::new().merge(SpaRouter::new("/", "web/dist").index_file("index.html"));
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
