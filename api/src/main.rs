1e028f1d46f9ffa056b1550cf3b52cf51b6cb500
sneakycrow <zach@sneakycrow.dev>
2022-12-24 08:44:53 -0800

    init upload route

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,14 @@
 use std::net::SocketAddr;
 
-use axum::Router;
+use axum::{routing::get, Router};
 use axum_extra::routing::SpaRouter;
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
-    let app: Router = Router::new().merge(SpaRouter::new("/", "web/dist").index_file("index.html"));
+    let index = SpaRouter::new("/", "web/dist").index_file("index.html");
+    let app: Router = Router::new().merge(index).route("/upload", get(upload));
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -17,3 +18,5 @@ async fn main() {
         .await
         .unwrap();
 }
+
+async fn upload() {}
