c2d9a82ad13216fdf647a6930943e9f0f90c3292
sneakycrow <zach@sneakycrow.dev>
2023-01-10 10:10:37 -0800

    init upload page

---
+++ web/src/Upload.tsx
@@ -0,0 +1,3 @@
+const Upload = () => <h1>Contact page</h1>
+
+export default Upload;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import Root from './Root'
 import './index.css'
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import Upload from "./Upload";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />
+    },
+    {
+        path: "/upload",
+        element: <Upload />
     }
 ]);
 
